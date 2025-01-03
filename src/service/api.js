4a13f99c3ae53acc86b8b15342d984ecfd47a228
LotteEliasson <lottefacereliasson@hotmail.com>
2025-01-03 13:11:00 +0100

    url ændret

--- src/service/api.js
+++ src/service/api.js
@@ -2,8 +2,13 @@
 //Axois instance that include the JWT token in the Authorization header.
 
 import axios from "axios";
+//local
+//const API_URL = 'http://localhost:5000/api';
 
-const API_URL = 'http://localhost:5000/api';
+//azure
+const API_URL = 'https://electricparts-h8a0e7cec9d8fjb3.germanywestcentral-01.azurewebsites.net/api';
+
+
 
 const api = axios.create({ baseURL: API_URL });
 
